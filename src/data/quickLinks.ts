c2153f49322d4ce87b632365915ac270490ca99e
Staycoo10 <141923746+Staycoo10@users.noreply.github.com>
2026-06-10 15:04:08 +0300

    +Img

--- src/data/quickLinks.ts
+++ src/data/quickLinks.ts
@@ -10,7 +10,7 @@ export const mockQuickLinks: QuickLink[] = [
     id: 'ql1', 
     title: 'Despre noi', 
     path: '/despre-noi', 
-    image: 'https://scontent.fkiv7-1.fna.fbcdn.net/v/t39.30808-6/494268671_1266317955500334_145360501761658815_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=106&ccb=1-7&_nc_sid=7b2446&_nc_ohc=CAF_tXhnV3EQ7kNvwGOZCZ_&_nc_oc=Adl8er0WXPCJjInmBwpjtwzd3At7Ghdkwecl1ouayTKRF2CnZiIVVV1IJ9chhEwB7og&_nc_zt=23&_nc_ht=scontent.fkiv7-1.fna&_nc_gid=j-2tOl45NlNjo0aWEBAaOA&_nc_ss=8&oh=00_Afyi0rNSYerKH8bcbfkxcPA6SiMumCpm26MpsYAX8D-6Mg&oe=69B8C2A3' 
+    image: 'https://i.pcmag.com/imagery/articles/07IDqpm1hTlSNl1Eamwz700-1.fit_lim.size_1600x900.v1569488958.jpg' 
   },
   { 
     id: 'ql2', 
